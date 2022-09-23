6e11349b1f9cec7a0e85d38af960ed98bc005ab0
nikhilvn <nvnofficial13@gmail.com>
2022-09-23 18:30:05 +0100

    Added types and function to calculate checkout

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-console.log("Hello world");
+type Item = "Apple" | "Orange";
+
+const itemCosts = {
+  Apple: 60,
+  Orange: 25,
+};
+
+function calculateCheckoutAmount(cartItems: Item[]) {
+  let totalAmount = 0;
+  const itemsHash = {
+    Apple: 0,
+    Orange: 0,
+  };
+
+  for (let i = 0; i < cartItems.length; i++) {
+    itemsHash[cartItems[i]]++;
+  }
+
+  totalAmount =
+    itemsHash.Apple * itemCosts.Apple + itemsHash.Orange * itemCosts.Orange;
+
+  return totalAmount;
+}
+
+const cart: Item[] = ["Apple", "Apple", "Orange", "Apple"];
+const amount = calculateCheckoutAmount(cart);
+
+console.log(amount);
